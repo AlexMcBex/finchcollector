f6dd02b992b210995dbb32a5634bb32ad43f6f21
AlexMcBex <alessiobelli94@gmail.com>
2023-03-02 18:31:39 -0500

    set model and admin can create finches

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from .models import Finch
 
-#temporary finch
-finch=[
-    {'name': 'Bobi', 'color': 'blue', 'age': 5},
-    {'name': 'Kino', 'color': 'gray', 'age': 3}
-]
+# temporary finch
+# finch=[
+#     {'name': 'Bilu', 'color': 'blue', 'age': 5},
+#     {'name': 'Kino', 'color': 'gray', 'age': 3}
+# ]
 
 # Create your views here.
 def home(request):
@@ -14,4 +15,5 @@ def about(request):
     return render(request, 'about.html')
 
 def finch_index(request):
+    finch= Finch.objects.all()
     return render(request, 'finch/index.html', { 'finch': finch })
