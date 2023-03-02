26313978340ba4c726d4db14bed3088c68b95327
AlexMcBex <alessiobelli94@gmail.com>
2023-03-02 16:32:26 -0500

    set index page

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+#temporary finch
+finch=[
+    {'name': 'Bobi', 'color': 'blue', 'age': 5},
+    {'name': 'Kino', 'color': 'gray', 'age': 3}
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def finch_index(request):
+    return render(request, 'finch/index.html', { 'finch': finch })
