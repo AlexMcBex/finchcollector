b527672205e87b1e63b323c33780e92c31738b5f
AlexMcBex <alessiobelli94@gmail.com>
2023-03-04 16:06:43 -0500

    added ribbon model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+RIBBONS = (
+    ('G', 'Golden Ribbon'),
+    ('R', 'Red Ribbon'),
+    ('B', 'Blue Ribbon')
+)
+
 # Create your models here.
 class Finch(models.Model):
     name = models.CharField(max_length=100)
@@ -13,3 +19,15 @@ class Finch(models.Model):
     def get_absolute_url(self):
         return reverse("detail", kwargs={"finch_id": self.id})
     
+class Ribbon(models.Model):
+    date = models.DateField('Day of Gift')
+    color = models.CharField(
+        max_length=1,
+        choices=RIBBONS,
+        default=[0][0]  
+    )
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.get_color_display()}, gifted on {self.date}'
+
